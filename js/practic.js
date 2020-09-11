e914a60b6ec4411d1658de1ef8716db8613c3a12
Serhii Chyrko <64086212+Segijua06@users.noreply.github.com>
2020-09-12 02:03:36 +0300

    /

--- js/practic.js
+++ js/practic.js
@@ -3,9 +3,23 @@ const doMath = function (a, b, callback) {
 
     console.log(result);
 };
-
+/*
 const add = function (x, y) {
     return x + y;
 };
 
-doMath(3, 2,add);
+const sub = function (x, y) {
+    return x - y;
+};
+
+doMath(3, 2, add);
+doMath(10, 4, sub);
+*/ 
+
+doMath(3, 2, function (x, y) {
+    return x + y;
+});
+
+doMath(10, 4, function (x, y) {
+    return x - y;
+});
