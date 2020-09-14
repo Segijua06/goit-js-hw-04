42759a3be27e1205e9b3badd3890322163251079
Serhii Chyrko <64086212+Segijua06@users.noreply.github.com>
2020-09-14 23:35:35 +0300

    .

--- js/practic.js
+++ js/practic.js
@@ -1,25 +1,47 @@
-const doMath = function (a, b, callback) {
-    const result = callback(a,b);
+// const doMath = function (a, b, callback) {
+//     const result = callback(a,b);
 
-    console.log(result);
-};
-/*
-const add = function (x, y) {
-    return x + y;
-};
+//     console.log(result);
+// };
+// /*
+// const add = function (x, y) {
+//     return x + y;
+// };
 
-const sub = function (x, y) {
-    return x - y;
-};
+// const sub = function (x, y) {
+//     return x - y;
+// };
 
-doMath(3, 2, add);
-doMath(10, 4, sub);
-*/ 
+// doMath(3, 2, add);
+// doMath(10, 4, sub);
+// */ 
 
-doMath(3, 2, function (x, y) {
-    return x + y;
-});
+// doMath(3, 2, function (x, y) {
+//     return x + y;
+// });
 
-doMath(10, 4, function (x, y) {
-    return x - y;
-});
+// doMath(10, 4, function (x, y) {
+//     return x - y;
+// });
+
+
+/* 
+    Отложеный вызов: интервалы
+*/
+
+// const callback = function () {
+//     console.log('Через 2 секунды внутри колбека в таймауте');
+// };
+
+// console.log('В коде перед таймаутом');
+
+// setTimeout(callback, 2000);
+
+// console.log('В коде после таймаутом')
+
+
+/* 
+    ФИльтр
+*/
+
+const filter = function (array, test) {};
